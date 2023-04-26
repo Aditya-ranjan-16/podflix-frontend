996ee693bade93922455a5d44780052d438f6447
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-26 13:20:26 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import NCss from "./Css/Nav.module.css";
 
 // icons
+import FitbitIcon from "@mui/icons-material/Fitbit";
 import HomeIcon from "@mui/icons-material/Home";
 import MicNoneIcon from "@mui/icons-material/MicNone";
 import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
@@ -14,21 +15,28 @@ import SearchIcon from "@mui/icons-material/Search";
 export default function Nav() {
   return (
     <div className={NCss.mDiv}>
-      <Link to="/" className="LinkStyle">
-        <HomeIcon />
-      </Link>
-      <Link to="/Audio" className="LinkStyle">
-        <MicNoneIcon />
-      </Link>
-      <Link to="/Videos" className="LinkStyle">
-        <OndemandVideoIcon />
-      </Link>
-      <Link to="/Library" className="LinkStyle">
-        <LocalLibraryIcon />
-      </Link>
-      <Link to="/Search" className="LinkStyle">
-        <SearchIcon />
-      </Link>
+      <div className={NCss.innerDiv}>
+        <div className={NCss.LogoDiv}>
+          <Link to="/" className="LinkStyle">
+            <FitbitIcon fontSize="large" />
+          </Link>
+        </div>
+        <Link to="/" className="LinkStyle">
+          <HomeIcon />
+        </Link>
+        <Link to="/Audio" className="LinkStyle">
+          <MicNoneIcon />
+        </Link>
+        <Link to="/Videos" className="LinkStyle">
+          <OndemandVideoIcon />
+        </Link>
+        <Link to="/Library" className="LinkStyle">
+          <LocalLibraryIcon />
+        </Link>
+        <Link to="/Search" className="LinkStyle">
+          <SearchIcon />
+        </Link>
+      </div>
     </div>
   );
 }
