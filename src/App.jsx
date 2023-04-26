f99f8367824b564ca28b1dfc2eb3ca303d66a054
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-26 12:06:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useContext, Suspense } from "react";
 
 // Pages
-const Home = React.lazy(() => import("./pages/Home"));
+const Home = React.lazy(() => import("./Pages/Home"));
 const Error = React.lazy(() => import("./pages/Error"));
 
-//
+// Components
 import Footer from "./Components/Footer";
 
 // Loading
