aba759319efd1477a61dc612357d90eff00a1e62
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-26 14:21:27 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,71 +4,76 @@ import React from "react";
 import Nav from "../Components/Nav";
 import Header from "../Components/Home/Header";
 
+// Css
+import "./Css/Commom.css";
+
 export default function Home() {
   return (
     <div>
       <Nav />
       <Header />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
-      ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
-      neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
-      aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt et
-      saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
-      beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
-      tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium nisi
-      omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
-      dignissimos cupiditate enim libero sapiente ratione quia accusamus harum?
-      Tenetur quis voluptatum atque eum maxime, eligendi ullam animi. Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
-      ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
-      neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
-      aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt et
-      saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
-      beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
-      tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium nisi
-      omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
-      dignissimos cupiditate enim libero sapiente ratione quia accusamus harum?
-      Tenetur quis voluptatum atque eum maxime, eligendi ullam animi. Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
-      ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
-      neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
-      aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt et
-      saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
-      beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
-      tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium nisi
-      omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
-      dignissimos cupiditate enim libero sapiente ratione quia accusamus harum?
-      Tenetur quis voluptatum atque eum maxime, eligendi ullam animi. Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
-      ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
-      neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
-      aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt et
-      saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
-      beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
-      tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium nisi
-      omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
-      dignissimos cupiditate enim libero sapiente ratione quia accusamus harum?
-      Tenetur quis voluptatum atque eum maxime, eligendi ullam animi. Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
-      ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
-      neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
-      aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt et
-      saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
-      beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
-      tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium nisi
-      omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
-      dignissimos cupiditate enim libero sapiente ratione quia accusamus harum?
-      Tenetur quis voluptatum atque eum maxime, eligendi ullam animi. Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
-      ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
-      neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
-      aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt et
-      saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
-      beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
-      tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium nisi
-      omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
-      dignissimos cupiditate enim libero sapiente ratione quia accusamus harum?
-      Tenetur quis voluptatum atque eum maxime, eligendi ullam animi.
+      <div className="ContentNavBorder">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
+        ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
+        neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
+        aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt
+        et saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
+        beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
+        tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium
+        nisi omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
+        dignissimos cupiditate enim libero sapiente ratione quia accusamus
+        harum? Tenetur quis voluptatum atque eum maxime, eligendi ullam animi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
+        ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
+        neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
+        aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt
+        et saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
+        beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
+        tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium
+        nisi omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
+        dignissimos cupiditate enim libero sapiente ratione quia accusamus
+        harum? Tenetur quis voluptatum atque eum maxime, eligendi ullam animi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
+        ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
+        neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
+        aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt
+        et saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
+        beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
+        tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium
+        nisi omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
+        dignissimos cupiditate enim libero sapiente ratione quia accusamus
+        harum? Tenetur quis voluptatum atque eum maxime, eligendi ullam animi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
+        ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
+        neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
+        aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt
+        et saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
+        beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
+        tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium
+        nisi omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
+        dignissimos cupiditate enim libero sapiente ratione quia accusamus
+        harum? Tenetur quis voluptatum atque eum maxime, eligendi ullam animi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
+        ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
+        neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
+        aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt
+        et saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
+        beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
+        tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium
+        nisi omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
+        dignissimos cupiditate enim libero sapiente ratione quia accusamus
+        harum? Tenetur quis voluptatum atque eum maxime, eligendi ullam animi.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem praesentium
+        ducimus, qui corrupti voluptatem vel cum cupiditate. Provident velit a
+        neque dolor, ab ex. Nam ullam provident, beatae facilis quo ex corporis
+        aut accusantium nisi illo, praesentium consequuntur nemo rem. Deserunt
+        et saepe cupiditate optio, quas excepturi ullam placeat aliquid a illum
+        beatae! Dolor nemo nobis asperiores non quisquam doloremque deleniti
+        tempore, quaerat minima repellat cumque? Cumque cum ea ut accusantium
+        nisi omnis totam dolorem quasi rerum fuga quidem ex recusandae, labore
+        dignissimos cupiditate enim libero sapiente ratione quia accusamus
+        harum? Tenetur quis voluptatum atque eum maxime, eligendi ullam animi.
+      </div>
     </div>
   );
 }
