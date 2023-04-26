6063ea8ab91390bf142a943ab38afaff06a8d2aa
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-26 12:56:59 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,6 +3,13 @@ import React from "react";
 // Css
 import NCss from "./Css/Nav.module.css";
 
+// icons
+import HomeIcon from "@mui/icons-material/Home";
+
 export default function Nav() {
-  return <div className={NCss.mDiv}>Nav</div>;
+  return (
+    <div className={NCss.mDiv}>
+      <HomeIcon />
+    </div>
+  );
 }
