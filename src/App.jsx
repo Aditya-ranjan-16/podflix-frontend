511ea6745f29add2073803b44f9571803dc58072
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-26 11:39:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import React, { useContext, Suspense } from "react";
+
+// Pages
+const IndexMain = React.lazy(() => import("./pages/IndexMain"));
+
 function App() {
   return <div>Hello World</div>;
 }
