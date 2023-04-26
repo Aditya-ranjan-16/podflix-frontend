2ba40b248463d025c6c92bc71f7735c6b9d5d4ff
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-26 12:06:59 +0530

    Update Header.jsx

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import Sliders from "react-slick";
+
+// css
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import HeaderCss from "./Css/Header.module.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  var settings = {
+    dots: false,
+    infinite: true,
+    arrows: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return (
+    <div className={HeaderCss.SlideShow}>
+      <div className={HeaderCss.slideDiv}>
+        <Sliders {...settings} className={HeaderCss.Hslide}>
+          <div className={HeaderCss.slide}>
+            <img src={img1} className={HeaderCss.ImgHeader} />
+          </div>
+          <div className={HeaderCss.slide}>
+            <img src={img2} className={HeaderCss.ImgHeader} />
+          </div>
+          <div className={HeaderCss.slide}>
+            <img src={img3} className={HeaderCss.ImgHeader} />
+          </div>
+        </Sliders>
+      </div>
+    </div>
+  );
 }
