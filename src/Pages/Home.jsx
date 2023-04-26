100fda25b0faedf1d6909df8eb16498be312c03e
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-26 12:52:15 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 // Components
+import Nav from "../Components/Nav";
 import Header from "../Components/Home/Header";
 
 export default function Home() {
   return (
     <div>
+      <Nav />
       <Header />
     </div>
   );
